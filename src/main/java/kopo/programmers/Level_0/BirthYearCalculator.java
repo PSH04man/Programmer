637d6feb169ede0ac4d8e043423199459fbaaf8a
QKRTJDUGS <bagseonghuyn0505@gmail.com>
2026-07-03 19:06:03 +0900

    BirthYearCalculator  (선생님 나이로 출생년도 계산하기)

---
+++ src/main/java/kopo/programmers/Level_0/BirthYearCalculator.java
@@ -0,0 +1,18 @@
+package main.java.kopo.programmers.Level_0;
+
+public class BirthYearCalculator {
+    public static void main(String[] args) {
+        /*
+        머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
+        2022년 기준 선생님의 나이 age가 주어질 때,
+        선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+         */
+
+        int answer = solution(40);
+        System.out.print(answer);
+    }
+        public static  int solution(int age) {
+            return 2022 - age + 1;
+
+    }
+}
---
+++ src/main/java/kopo/programmers/Level_0/RemainderCalculator.java
@@ -0,0 +1,20 @@
+package main.java.kopo.programmers.Level_0;
+
+public class RemainderCalculator {
+    public static void main(String[] args) {
+        /*
+        정수 num1, num2가 매개변수로 주어질 때,
+        num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+         */
+    }
+
+    static class Solution {
+        public int solution(int num1, int num2) {
+            int answer = num1 %  num2;
+            return answer;
+        }
+
+        // 바로 여기서 나누기 해버리기.
+
+    }
+}
